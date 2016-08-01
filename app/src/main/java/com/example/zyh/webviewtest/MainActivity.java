d37f4de0e404cd9d57dbfc8aa1894a887d61c64d
zhuyuhui1234 <949872522@qq.com>
2016-08-01 23:43:08 +0800

    third commit

--- app/src/main/java/com/example/zyh/webviewtest/MainActivity.java
+++ app/src/main/java/com/example/zyh/webviewtest/MainActivity.java
@@ -1,24 +1,29 @@
 package com.example.zyh.webviewtest;
 
 import android.app.Activity;
+import android.content.ComponentName;
 import android.content.Intent;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.KeyEvent;
+import android.view.View;
 import android.view.Window;
+import android.webkit.JavascriptInterface;
 import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-       public  static  String url1="http://www.baidu.com";
+    public  static  String url1="http://www.baidu.com";
     public  static  String url2="http://www.4399.com";
     public  static  String url3="http://my.csdn.net";
     final Activity activity=this;
     public  WebView webView;
     public  WebSettings webSetting;
+    public Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,19 +32,21 @@ public class MainActivity extends AppCompatActivity {
         Intent intent =new Intent(Intent.ACTION_VIEW,uri);
         startActivity(intent);　　*/
 
-
-
         setContentView(R.layout.activity_main);
+        button=(Button)findViewById(R.id.button);
         webView=(WebView)findViewById(R.id.web);
         webSetting=webView.getSettings();
+
+
         webSetting.setJavaScriptEnabled(true);//启用Ｊｓ建议开启
         webSetting.setBuiltInZoomControls(true);//内置缩放大小功能，建议开启
         webSetting.setLoadsImagesAutomatically(true);//支持自动加载图片　　　建议开启
         webSetting.setLoadWithOverviewMode(true);
-        webSetting.setTextZoom(50);//设置文本大小缩放程度，默认１００，不缩放，启用缩放可能可以看到更多文字
+        webSetting.setTextZoom(200);//设置文本大小缩放程度，默认１００，不缩放，启用<100缩放可能可以看到更多文字
         webSetting.setUseWideViewPort(true);   //设置视窗，开启可以看到更多信息，有点类似以电脑状态访问界面
         webSetting.setLayoutAlgorithm(WebSettings.LayoutAlgorithm.NORMAL);//自适应屏幕，有四个值，目前没发现区别，先使用Normal
         webSetting.setSaveFormData(false);//保存表单数据
+
         /*
         * 重写此方法，自己加载网页；返回true当前主机程序处理，
         * return false当前Webview处理
@@ -51,13 +58,21 @@ public class MainActivity extends AppCompatActivity {
                 return  false;
             }
 
-
         });
 
-             webView.loadUrl(url1);
+        webView.loadUrl("file:///android_asset/demo.html");
 
 
+        webView. addJavascriptInterface(new JsObject(),"lala");
+                button.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        webView.loadUrl("javascript:fillContent()");
+                    }
+                });
     }
+
+
     /*
     *开启返回功能，即按下返回键返回上一个网页，
     *若没用，则退出当前Activity
@@ -75,5 +90,18 @@ public class MainActivity extends AppCompatActivity {
             }
         }
         return super.onKeyDown(KeyCode,event);
+    }
+
+
+    public  class  JsObject {
+        @JavascriptInterface
+        public void startMap() {
+            Intent mIntent = new Intent();
+            ComponentName component = new ComponentName("com.google.android.apps.maps",
+                    "com.google.android.maps.MapsActivity");
+            mIntent.setComponent(component);
+
+            startActivity(mIntent);
+        }
     }
 }
